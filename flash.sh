208d98ca6ae59fa5658420e64c50a7d5c4dac510
Dustin Schultz <dustindschultz@gmail.com>
2024-06-15 14:05:26 -0400

    configured for rp2040

--- flash.sh
+++ flash.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-SIDE=${1:-left}
 
-qmk flash \
-    -kb handwired/dactyl_manuform/5x7_1 \
-    -bl PICO_PLATFORM=rp2040 \
-    -e SIDE=$SIDE \
-    -e CONVERT_TO=promicro_rp2040 \
-    keymap.json \
-    && \
+qmk compile -kb handwired/dactyl_manuform/5x7_1 ./keymap.json &&\
+sudo picotool load .build/handwired_dactyl_manuform_5x7_1_default.uf2
 
-    cp ./.build/handwired_dactyl_manuform_5x7_handwired_dactyl_manuform_5x7_layout_5x7_mine_promicro_rp2040.uf2 \
-       ./dactyl_$SIDE.uf2 \
 
-    sudo picotool load ./dactyl_$SIDE.uf2
 
