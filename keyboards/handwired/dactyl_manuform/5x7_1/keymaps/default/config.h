0499bca90ce925be6c6f9c1ed2419a0a7ee9fe82
Dustin Schultz <dustindschultz@gmail.com>
2024-06-20 22:28:17 -0400

    set up for rp2040

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/config.h
@@ -20,17 +20,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_RIGHT
 // #define DEBOUNCE 5
 
-// #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode
 // // #define SERIAL_USART_HALF_DUPLEX   // Enable half duplex operation mode
-// #define SERIAL_USART_RX_PIN GP0
-// #define SERIAL_USART_TX_PIN GP1
+#define SERIAL_USART_RX_PIN GP0
+#define SERIAL_USART_TX_PIN GP1
 // #define SERIAL_USART_TX_PIN GP0
 
-#define SPLIT_HAND_PIN GP26 // Shunt from 3v to GP26 on left side
+#define SPLIT_HAND_PIN GP8 // Shunt from 3v to GP26 on left side
+// #define	SPLIT_HAND_PIN_LOW_IS_LEFT
+
+// #define USE_SERIAL
+
+// #define SOFT_SERIAL_PIN D0
 
 // #define SPLIT_USB_DETECT
 // #define SOFT_SERIAL_PIN GP0
 
-
-// #define ENCODERS_PAD_A { GP27 }
-// #define ENCODERS_PAD_B { GP28 }
