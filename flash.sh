d9bd8668d4713fed67dbab30effb72b4f1b63a7a
Dustin Schultz <dustindschultz@gmail.com>
2024-07-04 19:53:21 -0400

    switched back to rp2040

--- flash.sh
+++ flash.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 SIDE=${1:-left}
+PARAMS="flash"
 
-if [ "$SIDE" = "left" ]; then
-    qmk compile -kb handwired/dactyl_manuform/5x7_1 ./keymap.json &&\
-    sudo picotool load .build/handwired_dactyl_manuform_5x7_1_default.uf2
-else
-    qmk compile -kb handwired/dactyl_manuform/5x7_1 ./keymap.json &&\
-    sudo picotool load .build/handwired_dactyl_manuform_5x7_1_default.uf2
+if [ -n "$SIDE" ]; then
+    PARAMS="-v flash -bl uf2-split-$SIDE"
 fi
 
-# qmk compile -kb handwired/jankrp2040dactyl ~/Downloads/handwired_jankrp2040dactyl_testes.json
-# sudo picotool load -f ./handwired_jankrp2040dactyl_handwired_jankrp2040dactyl_testes.uf2
+echo $PARAMS
 
-# qmk compile -kb ziptyze/lets_split_v3 /home/dustin/Downloads/ziptyze_lets_split_v3_testes.json
-# sudo picotool load -f ./ziptyze_lets_split_v3_ziptyze_lets_split_v3_testes.uf2
-
+qmk $PARAMS
 
