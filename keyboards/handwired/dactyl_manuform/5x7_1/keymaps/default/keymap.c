72e33232e03a3518c0bc757adf5a6804c3a6d77a
Dustin Schultz <dustindschultz@gmail.com>
2024-07-06 20:16:47 -0400

    both sides work with rp2040, updated keymap

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/keymap.c
@@ -20,17 +20,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,        KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_NO,
         KC_TAB,        KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_LBRC,
         KC_ESC,        KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   TAB_L,
-        OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_NO,
+        OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_MS_WH_UP,
         KC_CAPS,       KC_LGUI, TT(_FN), KC_NO,
                                                     KC_SPC, KC_BSPC,
-                                                    KC_LGUI, KC_LCTL,
+                                                    KC_LCTL, KC_LGUI,
                                                     KC_LALT, TT(_FN),
         // right hand
-        TG(_NUMPAD), KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC,
-        KC_RBRC,     KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
-        TAB_R,       KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-        KC_NO,       KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),
-                                       KC_MINS,  KC_EQL,   KC_NO,    KC_NO,
+        TG(_NUMPAD),   KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC,
+        KC_RBRC,       KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
+        TAB_R,         KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+        KC_MS_WH_DOWN, KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),
+                                         KC_MINS,  KC_EQL,   KC_NO,    KC_NO,
         KC_ENT,  KC_SPC,
         KC_RCTL, KC_RGUI,
         TT(_FN), KC_RALT
@@ -83,10 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [1] = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),           ENCODER_CCW_CW(RGB_SAD, RGB_SAI)  },
-    [2] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI),           ENCODER_CCW_CW(RGB_SPD, RGB_SPI)  },
-    [3] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD),          ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
+    [_QWERTY] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)  },
+    [_FN]     = { ENCODER_CCW_CW(KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP),  ENCODER_CCW_CW(KC_MS_WH_LEFT, KC_MS_WH_RIGHT)  },
+    [_NUMPAD] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD), ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
 };
 #endif
 
