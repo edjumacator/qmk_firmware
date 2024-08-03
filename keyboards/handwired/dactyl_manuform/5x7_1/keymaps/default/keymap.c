b76596acddcf98e2e98bc78be2fba096938a318a
Dustin Schultz <dustindschultz@gmail.com>
2024-08-03 04:10:53 -0400

    filled out default qwerty keymap

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/keymap.c
@@ -22,20 +22,20 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7_1(
         // left hand
-        KC_GRV,    KC_1,   KC_2,    KC_3,   KC_4,   KC_5,   TG(_CMD),
-        KC_TAB,    KC_Q,   KC_W,    KC_E,   KC_R,   KC_T,   TG(_NUMPAD),
-        KC_ESC,    KC_A,   KC_S,    KC_D,   KC_F,   KC_G,   KC_HOME,
-        KC_LSFT,   KC_Z,   KC_X,    KC_C,   KC_V,   KC_B,   KC_END,
-        KC_CAPS,   KC_NO,  KC_LBRC, KC_RBRC,
-                                                    OSL(_CMD), KC_LCTL,
-                                                    KC_LALT,   KC_LGUI,
-                                                    TT(_FN),   TT(_NUMPAD),
+        KC_GRV,    KC_1,     KC_2,    KC_3,   KC_4,   KC_5,   TG(_CMD),
+        KC_TAB,    KC_Q,     KC_W,    KC_E,   KC_R,   KC_T,   TG(_NUMPAD),
+        KC_ESC,    KC_A,     KC_S,    KC_D,   KC_F,   KC_G,   KC_HOME,
+        KC_LSFT,   KC_Z,     KC_X,    KC_C,   KC_V,   KC_B,   KC_END,
+        KC_CAPS,   TG(_FN),  KC_LBRC, KC_RBRC,
+                                                      OSL(_CMD),   KC_LCTL,
+                                                      KC_LALT,     KC_LGUI,
+                                                      TT(_NUMPAD), TT(_FN),
         // right hand
         KC_PRINT_SCREEN,  KC_6,  KC_7,   KC_8,     KC_9,     KC_0,     KC_LBRC,
         KC_PGUP,          KC_Y,  KC_U,   KC_I,     KC_O,     KC_P,     KC_RBRC,
         KC_PGDN,          KC_H,  KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         TAB_R,            KC_N,  KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                                         KC_MINS,  KC_EQL,   KC_BSLS,  KC_NO,
+                                         KC_MINS,  KC_EQL,   KC_BSLS,  TO(_CMD),
         KC_SPC,   KC_ENT,
         KC_BSPC,  KC_RCTL,
         KC_RGUI,  KC_RALT
@@ -51,11 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                  _______, _______,
                                                                  _______, _______,
                                                                  _______, _______,
-
         // right hand
         KC_F11,    _______,   _______,   _______,   _______,   _______,   QK_BOOT,
         KC_F12,    KC_F6,     KC_F7,      KC_F8,    KC_F9,     KC_F10,    _______,
-        KC_F13,    _______,   _______,    KC_UP,    _______,   _______,   _______,
+        KC_F13,    TAB_L,     _______,    KC_UP,    TAB_R,     _______,   _______,
         KC_F14,    _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,   _______,   _______,
                                          _______,   _______,   _______,   _______,
         KC_DEL,    _______,
@@ -65,14 +64,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUMPAD] = LAYOUT_5x7_1(
         // left hand
-        _______,      _______,   _______,   _______,   _______,   _______,  _______,
+        _______,      _______,   _______,   _______,   _______,   _______,  KC_CALC,
         _______,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,     _______,
         TO(_QWERTY),  _______,   _______,   _______,   _______,   _______,  _______,
         _______,      _______,   _______,   _______,   _______,   _______,  _______,
         _______,      _______,   _______,   _______,
-                                                               _______,  _______,
-                                                               _______,  _______,
-                                                               _______,  _______,
+                                                                  _______,  _______,
+                                                                  _______,  _______,
+                                                                  _______,  _______,
 
         // right hand
         _______,   _______,   KC_NUM,    _______,   KC_PMNS,   KC_PPLS,   _______,
@@ -92,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(_QWERTY),  _______,   _______,   _______,   _______,   _______,  QK_DYNAMIC_MACRO_PLAY_1,
         _______,      _______,   _______,   _______,   _______,   _______,  _______,
         _______,      _______,   _______,   _______,
-                                                               _______, _______,
-                                                               _______, _______,
-                                                               _______, _______,
+                                                                  _______, _______,
+                                                                  _______, _______,
+                                                                  _______, _______,
 
         // right hand
         QK_DYNAMIC_MACRO_RECORD_START_2,  _______,   _______,   _______,   _______,   _______,   _______,
@@ -111,8 +110,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_QWERTY] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)  },
-    [_FN]     = { ENCODER_CCW_CW(KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP),  ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) },
-    [_NUMPAD] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD), ENCODER_CCW_CW(TAB_L, TAB_R) },
+    [_FN]     = { ENCODER_CCW_CW(KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP),  ENCODER_CCW_CW(TAB_L, TAB_R) },
+    [_NUMPAD] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD), ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) },
     [_CMD]    = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD), ENCODER_CCW_CW(KC_BSPC, KC_SPC) },
 };
 #endif
