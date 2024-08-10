dcf2cb9e92064c428fd40c5222dabf7cab6c3e71
Dustin Schultz <dustindschultz@gmail.com>
2024-08-09 20:07:11 -0400

    set up mouse control layer and toggle key

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/keymap.c
@@ -7,13 +7,14 @@
 #define _QWERTY 0
 #define _FN     1
 #define _NUMPAD 2
-#define _CMD 3
+#define _CMD    3
+#define _MOUSE  4
 
 // Some basic macros
-#define TASK    LCTL(LSFT(KC_ESC))
-#define TAB_R   LCTL(KC_TAB)
-#define TAB_L   LCTL(LSFT(KC_TAB))
-#define TAB_RO  LCTL(LSFT(KC_T))
+#define TASK   LCTL(LSFT(KC_ESC))
+#define TAB_R  LCTL(KC_TAB)
+#define TAB_L  LCTL(LSFT(KC_TAB))
+#define TAB_RO LCTL(LSFT(KC_T))
 
 enum custom_keycodes {
     DBL_SPC = SAFE_RANGE,
@@ -22,17 +23,17 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7_1(
         // left hand
-        KC_GRV,    KC_1,     KC_2,    KC_3,   KC_4,   KC_5,   TG(_CMD),
-        KC_TAB,    KC_Q,     KC_W,    KC_E,   KC_R,   KC_T,   TG(_NUMPAD),
-        KC_ESC,    KC_A,     KC_S,    KC_D,   KC_F,   KC_G,   KC_HOME,
-        KC_LSFT,   KC_Z,     KC_X,    KC_C,   KC_V,   KC_B,   KC_END,
-        KC_CAPS,   TG(_FN),  KC_LBRC, KC_RBRC,
+        KC_GRV,    KC_1,        KC_2,    KC_3,   KC_4,   KC_5,   TG(_CMD),
+        KC_TAB,    KC_Q,        KC_W,    KC_E,   KC_R,   KC_T,   TG(_NUMPAD),
+        KC_ESC,    KC_A,        KC_S,    KC_D,   KC_F,   KC_G,   KC_HOME,
+        KC_LSFT,   KC_Z,        KC_X,    KC_C,   KC_V,   KC_B,   KC_END,
+        KC_CAPS,   TG(_MOUSE),  KC_LBRC, KC_RBRC,
                                                       OSL(_CMD),   KC_LCTL,
                                                       KC_LALT,     KC_LGUI,
                                                       TT(_NUMPAD), TT(_FN),
         // right hand
         KC_PRINT_SCREEN,  KC_6,  KC_7,   KC_8,     KC_9,     KC_0,     KC_LBRC,
-        KC_NO,            KC_Y,  KC_U,   KC_I,     KC_O,     KC_P,     KC_RBRC,
+        KC_MS_BTN1,       KC_Y,  KC_U,   KC_I,     KC_O,     KC_P,     KC_RBRC,
         KC_PGUP,          KC_H,  KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_PGDN,          KC_N,  KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                                          KC_MINS,  KC_EQL,   KC_BSLS,  TG(_CMD),
@@ -105,14 +106,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   _______,
         TT(_FN),   TT(_NUMPAD)
     ),
+
+    [_MOUSE] = LAYOUT_5x7_1(
+        // left hand
+        _______,      _______,     _______,   _______,   _______,     _______,     _______,
+        _______,      _______,     _______,   _______,   _______,     _______,     _______,
+        TO(_QWERTY),  _______,     _______,   _______,   KC_MS_BTN2,  KC_MS_BTN1,  _______,
+        _______,      _______,     _______,   _______,   _______,     _______,     _______,
+        _______,      TG(_MOUSE),  _______,   _______,
+                                                                  _______, _______,
+                                                                  _______, _______,
+                                                                  _______, _______,
+
+        // right hand
+        _______,  _______,  _______,    _______,    _______,   _______,   _______,
+        _______,  _______,  _______,    _______,    _______,   _______,   _______,
+        _______,  _______,  KC_MS_BTN1, KC_MS_BTN2, _______,   _______,   _______,
+        _______,  _______,  _______,    _______,    _______,   _______,   _______,
+                                        _______,    _______,   _______,   _______,
+        _______,   _______,
+        _______,   _______,
+        TT(_FN),   TT(_NUMPAD)
+    ),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_QWERTY] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)  },
+    [_QWERTY] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),                      ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)  },
     [_FN]     = { ENCODER_CCW_CW(KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP),  ENCODER_CCW_CW(TAB_L, TAB_R) },
-    [_NUMPAD] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD), ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) },
-    [_CMD]    = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD), ENCODER_CCW_CW(KC_BSPC, KC_SPC) },
+    [_NUMPAD] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD),                     ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) },
+    [_CMD]    = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD),                     ENCODER_CCW_CW(KC_BSPC, KC_SPC) },
+    [_MOUSE]  = { ENCODER_CCW_CW(KC_MS_UP, KC_MS_DOWN),                  ENCODER_CCW_CW(KC_MS_LEFT, KC_MS_RIGHT) },
 };
 #endif
 
