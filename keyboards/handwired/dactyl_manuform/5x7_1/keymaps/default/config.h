b0e1a929d5bf62102bcea94a7ba065ddd5e87d07
Dustin Schultz <dustindschultz@gmail.com>
2024-08-10 20:51:01 -0400

    added mouse control buttons to rotary encoder layer

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/config.h
@@ -22,3 +22,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SERIAL_USART_FULL_DUPLEX // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN GP0  // USART TX pin
 #define SERIAL_USART_RX_PIN GP1  // USART RX pin
+
+#define MK_COMBINED
