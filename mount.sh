927d9e58bcdcd8c3c2172838eb88bf7a816e5960
Dustin Schultz <dustindschultz@gmail.com>
2025-02-23 11:49:46 -0500

    added polling behavior to mount script

--- mount.sh
+++ mount.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-DEVICE=${1:-sdb1}
 
+# Get the initial list of devices
+initial_devices=$(ls /dev/sd*[0-9] /dev/ttyACM*[0-9] 2>/dev/null)
 
-# while device does not exist, wait:
-while [ ! -b /dev/$DEVICE ]; do
-    sleep 0.2
+echo "Waiting for new devices..."
+
+while true; do
+    # Get the current list of devices
+    current_devices=$(ls /dev/sd*[0-9] /dev/ttyACM*[0-9] 2>/dev/null)
+
+    # Compare the current list with the initial list
+    new_devices=$(comm -13 <(echo "$initial_devices" | sort) <(echo "$current_devices" | sort))
+
+    if [ -n "$new_devices" ]; then
+        DEVICE=$(echo "$new_devices" | head -n 1 | sed 's/\/dev\///')
+        break
+    fi
+
+    # Wait before checking again
+    sleep 0.1
 done
+
 
 sleep 2
 
